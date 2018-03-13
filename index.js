a2f40711ad7220a2e185031323b379be2c5ed186
Joey Cozza <joeycozza@gmail.com>
2018-03-12 19:00:10 -0600

    viewWidth < text.length working. need viewWidth > text.length still

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
 module.exports = function(options = {}) {
   const text = options.text || '';
+  const viewWidth = options.viewWidth || text.length;
+  const fill = options.fill || ' ';
   const interval = options.interval || 100;
+  const fullTextFrames = options.fullTextFrames || 1;
 
-  const frames = new Array(5);
+  if (fullTextFrames > 1 && viewWidth < text.length) {
+    throw new Error('Unable to show full text when viewWidth is smaller than text.length');
+  }
+
+  //TODO: JOEY if viewWidth is set, then we need to change the text being added here
+  let frames = new Array(fullTextFrames);
   frames.fill(text);
 
-  for (let i = 0; i <= text.length; i++) {
-    frames.unshift(' '.repeat(i) + text.substring(0, text.length - i));
-    frames.push(text.substring(i) + ' '.repeat(i));
+  for (let i = 1; i <= text.length; i++) {
+    frames.unshift(fill.repeat(i) + text.substring(0, text.length - i));
+    frames.push(text.substring(i) + fill.repeat(i));
+  }
+
+  frames = frames.map(frame => {
+    if (viewWidth < text.length) {
+      return frame.substring(0, viewWidth);
+    }
+
+    if (viewWidth > text.length) {
+      return frame;
+    }
+    return frame;
+  });
+
+  if (viewWidth < text.length) {
+    frames = frames.slice(text.length - viewWidth)
   }
 
   return {
     interval,
     frames
   };
-}
+};
